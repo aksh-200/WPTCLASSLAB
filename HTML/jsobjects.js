cf980efd947c907e2e1455090a3ac8ba9e7a2733
aksh-200 <81736871+aksh-200@users.noreply.github.com>
2023-12-21 13:54:35 +0530

    Add files via upload

---
+++ HTML/jsobjects.js
@@ -0,0 +1,14 @@
+let obj = {
+
+    fname:"Akshay",
+     lname : "wagh",
+     address :{
+        city:"pune",
+        state:"Maharashtra",
+    },
+      
+   skills:["js", "java"]
+
+}
+
+console.log(obj.address.city);
